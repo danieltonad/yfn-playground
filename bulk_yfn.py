f89adb12b26f13330279212cc5e4100122ae7a35
Daniel Solarin <soladan2000@gmail.com>
2024-12-03 09:51:35 -0900

    chore: bullk yfn stunt

--- bulk_yfn.py
+++ bulk_yfn.py
@@ -14,21 +14,26 @@ def get_highest_weekly_volume_ever(assets: List[str]) -> List[Tuple[int, str]]:
                 ticker_data = data.xs(ticker, level=1, axis=1)
                 # Extract the 'Volume' column
                 if 'Volume' in ticker_data:
-                    volume_data = ticker_data['Volume']
-                    highest_volume = volume_data.max()
-                    highest_volume_date = volume_data.idxmax().to_pydatetime().strftime("%d-%m-%Y %H:%M:%S")
-                    results.append((highest_volume.item(), highest_volume_date))
-                else:
-                    results.append((False, None))
+                    volume_data = ticker_data['Volume'].dropna()
+                    if not volume_data.empty:
+                        volume_data = ticker_data['Volume']
+                        highest_volume = volume_data.max()
+                        highest_volume_date = volume_data.idxmax().to_pydatetime().strftime("%d-%m-%Y %H:%M:%S")
+                        results.append({ticker: (highest_volume.item(), highest_volume_date)})
+                    else:
+                        results.append({ticker: (False, None)})
             except KeyError:
-                results.append((False, None))
+                results.append({ticker: (False, None)})
 
     except Exception as err:
+        print(err)
+        # log =  Logger()
+        # log.app_log(title="HWVE_ERR", message=f"Unable to Fetch volume for assets: {assets}")
         results.append((False, None))
     return results
 
 # Example usage
-assets = ["BEKE","OMC","WSO.B","DKNG","PFG","BAM","AER","EDR","GPC","ESS","PKG","EC","STLD","WSM","MAA","SYF","APTV","EXPE","WLK","J","ZBRA","ULTA","CNP","FOXA","FOX","DG","NTRSO","NRG","NTRS","SSNC","TS","CFG","MAS","LUV","EXPD","ZTO","UAL","VRSN","JBHT","AVTR","TME","SNAP","RYAN","DKS","AVY","LINE","FDS","IHG","MANH","BURL","CTRA","SUI","L","CRBG","ENTG","HRL","EBR","EBR.B","JHX","LPLA","IP","GEN","FNF","VIV","PSTG"]
+assets = ["BEKE","OMC","WSO.B","DKNG","PFG","BAM","AER","EDR","GPC","ESS","PKG","EC","STLD","WSM","SYF/PB","SYF/PA","MAA/PI","MAA","SYF","APTV","EXPE","WLK","J","ZBRA","ULTA","CNP","FOXA","FOX","DG","NTRSO","NRG","NTRS","CFG/PH","CFG/PE","SSNC","TS","CFG","MAS","LUV","EXPD","ZTO","UAL","VRSN","JBHT","AVTR","TME","SNAP","RYAN","DKS","AVY","LINE","FDS","IHG","MANH","BURL","CTRA","SUI","L","CRBG","ENTG","HRL","EBR","EBR.B","JHX","LPLA","IP","GEN","FNF","VIV","PSTG"]
 results = get_highest_weekly_volume_ever(assets)
 for result in results:
     print(result)
